61e9024cdb586c5f348704e472fb2072107d967a
Kudo-Shinichi-1 <XiaoyaoSZU@163.com>
2024-12-15 21:43:33 +0800

    Update nav.js

--- cn/nav.js
+++ cn/nav.js
@@ -1,7 +1,7 @@
 document.writeln("		<nav>");
 document.writeln("    		<ul>");
 document.writeln("        		<li><a href=\"../cn/index.html\">主页</a></li>");
-document.writeln("        		<li><a href=\"scholar.html\">经历</a></li>");
+document.writeln("        		<li><a href=\"experience.html\">经历</a></li>");
 document.writeln("        		<li><a href=\"life.html\">生活</a></li>");
 document.writeln("    		</ul>");
 document.writeln("		</nav>");
