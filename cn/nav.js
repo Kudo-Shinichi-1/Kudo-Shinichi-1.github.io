8387871316351f3b5af2a5852e85c0aae0c81d0d
YinkaiYu <yuyk6@mail2.sysu.edu.cn>
2024-12-16 19:00:32 +0800

    添加了课程论文

--- cn/nav.js
+++ cn/nav.js
@@ -2,6 +2,7 @@ document.writeln("		<nav>");
 document.writeln("    		<ul>");
 document.writeln("        		<li><a href=\"../cn/index.html\">主页</a></li>");
 document.writeln("        		<li><a href=\"experience.html\">经历</a></li>");
+document.writeln("        		<li><a href=\"blog.html\">博客</a></li>");
 document.writeln("        		<li><a href=\"life.html\">生活</a></li>");
 document.writeln("    		</ul>");
 document.writeln("		</nav>");
